8f18cd76f74e67fcea10afdf2de0ddc9f93abd07
Martin Zugnoni <martin.zugnoni@gmail.com>
2015-12-29 18:47:35 -0300

    Set homepage URL

--- rmotr_campus/rmotr_campus/urls.py
+++ rmotr_campus/rmotr_campus/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = (
     # Third party apps
@@ -8,4 +9,5 @@ urlpatterns = (
 
     # Own apps
     url(r'^accounts/', include('accounts.urls')),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
 )
